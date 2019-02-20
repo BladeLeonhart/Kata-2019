b6c2e31a5a5055d8cd3c54ac12480d386934e909
Ivan Macho <ivixinzo@gmail.com>
2019-02-20 12:35:28 +0100

    15 devuelve FizzBuzz

--- src/com/cifpcarballeira/katas/FizzBuzz/FizzBuzz.java
+++ src/com/cifpcarballeira/katas/FizzBuzz/FizzBuzz.java
@@ -3,9 +3,11 @@ package com.cifpcarballeira.katas.FizzBuzz;
 public class FizzBuzz {
 
     public String getFizzBuzz(int num) {
-        
-        if (num%5==0) {
-        return "Buzz";
+        if (num % 5 == 0 && num % 3 == 0) {
+            return "FizzBuzz";
+        }
+        if (num % 5 == 0) {
+            return "Buzz";
         }
         if (num % 3 == 0) {
             return "Fizz";
--- test/com/cifpcarballeira/katas/FizzBuzz/tests/TestFizzBuzz.java
+++ test/com/cifpcarballeira/katas/FizzBuzz/tests/TestFizzBuzz.java
@@ -15,25 +15,32 @@ public class TestFizzBuzz {
         String result = fb.getFizzBuzz(1);
         assertEquals("1", result);
     }
-    
+
     @Test
     public void testdevuelve2() {
         FizzBuzz fb = new FizzBuzz();
         String result = fb.getFizzBuzz(2);
         assertEquals("2", result);
     }
-    
+
     @Test
     public void testdevuelveF() {
         FizzBuzz fb = new FizzBuzz();
         String result = fb.getFizzBuzz(3);
         assertEquals("Fizz", result);
     }
-    
-     @Test
+
+    @Test
     public void testdevuelveB() {
         FizzBuzz fb = new FizzBuzz();
         String result = fb.getFizzBuzz(5);
         assertEquals("Buzz", result);
+    }
+
+    @Test
+    public void testdevuelveFB() {
+        FizzBuzz fb = new FizzBuzz();
+        String result = fb.getFizzBuzz(15);
+        assertEquals("FizzBuzz", result);
     }
 }
